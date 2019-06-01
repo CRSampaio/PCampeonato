b6289ef586e81ed7c613ecb3d2404357ab52ff5d
JhowFerri <jhowvore@gmail.com>
2019-06-01 13:10:12 -0300

    subindo atualizações feitas pelo Vitor em 31/05/2019

--- PCampeonato/PCampeonato/Estadio.cs
+++ PCampeonato/PCampeonato/Estadio.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace PCampeonato
 {
     class Estadio
     {
+        private int idEstadio;
+        private string nomeEstadio;
+
+        public int IdEstadio
+        {
+            get { return idEstadio; }
+            set { idEstadio = value; }
+        }
+        public string NomeEstadio
+        {
+            get { return nomeEstadio; }
+            set { nomeEstadio = value; }
+        }
+        public DataTable Listar()
+        {
+            SqlDataAdapter daEstadio;
+            DataTable dtEstadio = new DataTable();
+            try
+            {
+                daEstadio = new SqlDataAdapter("SELECT * FROM TIME", frmMenuPrincipal.conexao);
+                daEstadio.Fill(dtEstadio)
+            }
+        }
+
+
     }
 }
--- PCampeonato/PCampeonato/Jogo.cs
+++ PCampeonato/PCampeonato/Jogo.cs
@@ -8,5 +8,12 @@ namespace PCampeonato
 {
     class Jogo
     {
+        private int idJogo;
+        private string nomeJogo;
+
+        public int IdJogo { get { return idJogo; } set { idJogo = value; } }
+        public string NomeJogo { get { return nomeJogo; } set { nomeJogo = value; } }
+
+        
     }
 }
