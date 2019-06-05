6e5e340f3800885a4a953f9dc60f947b14c4cf58
JhowFerri <jhowvore@gmail.com>
2019-06-04 21:35:44 -0300

    CLASSE ESTÁDIO PRONTA

--- PCampeonato/PCampeonato/Estadio.cs
+++ PCampeonato/PCampeonato/Estadio.cs
@@ -30,9 +30,70 @@ namespace PCampeonato
             DataTable dtEstadio = new DataTable();
             try
             {
-                daEstadio = new SqlDataAdapter("SELECT * FROM TIME", frmMenuPrincipal.conexao);
-                daEstadio.Fill(dtEstadio)
+                daEstadio = new SqlDataAdapter("SELECT * FROM ESTADIO", frmMenuPrincipal.conexao);
+                daEstadio.Fill(dtEstadio);
+                daEstadio.FillSchema(dtEstadio, SchemaType.Source);
             }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dtEstadio;
+        }
+
+        public int Salvar()
+        {
+            SqlCommand mycommand;
+            int nReg = 0;
+            int retorno = 0;
+            try
+            {
+                mycommand = new SqlCommand("INSERT INTO ESTADIO VALUES (@nome_estadio)", frmMenuPrincipal.conexao);
+                mycommand.Parameters.Add(new SqlParameter("@nome_estadio", SqlDbType.VarChar));
+                mycommand.Parameters["@nome_estadio"].Value = nomeEstadio;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return nReg;
+        }
+
+        public int Alterar()
+        {
+            int retorno = 0;
+            SqlCommand mycommand;
+            int nReg = 0;
+            mycommand = new SqlCommand("UPDATE ESTADIO SET NOME_ESTADIO = @nome_estadio WHERE ID_ESTADIO = @id_estadio", frmMenuPrincipal.conexao);
+            mycommand.Parameters.Add(new SqlParameter("@id_estadio", SqlDbType.Int));
+            mycommand.Parameters.Add(new SqlParameter("@nome_estadio", SqlDbType.VarChar));
+            mycommand.Parameters["@id_estadio"].Value = idEstadio;
+            mycommand.Parameters["@nome_estadio"].Value = nomeEstadio;
+
+            nReg = mycommand.ExecuteNonQuery();
+            if(nReg > 0)
+            {
+                retorno = nReg;
+            }
+            return nReg;
+        }
+
+        public int Excluir()
+        {
+            int nReg = 0;
+            SqlCommand mycommand;
+            try
+            {
+                mycommand = new SqlCommand("DELETE FROM ESTADIO WHERE id_estadio=@id_estadio", frmMenuPrincipal.conexao);
+                mycommand.Parameters.Add(new SqlParameter("@id_estadio",
+                    SqlDbType.Int));
+                mycommand.Parameters["@id_estadio"].Value = idEstadio;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return nReg;
         }
 
 
